b56fa9fdddbb98d692e75828856491f76bba5d2e
Nancy Ouyang <nouyang@alum.mit.edu>
2017-10-22 23:10:42 -0400

    It works, I just had workspace() non-cleared issues

--- wk5/Plots_rectangle.jl
+++ wk5/Plots_rectangle.jl
@@ -3,8 +3,8 @@ using GeometryTypes
 gr() 
 
 ### SANITY CHECK
-x = 1:10; y = rand(10,2) # 2 columns means two lines
-plot(x,y)
+#x = 1:10; y = rand(10,2) # 2 columns means two lines
+#plot(x,y)
 
 #rect = HyperRectangle(Vec(0.5, 0.5), Vec(1., 1))
 
@@ -18,18 +18,14 @@ plot(x,y)
 
 @recipe function f(r::HyperRectangle)
     points = decompose(Point{2,Float64}, r)
-    rectpoints = points[[1,2,4,3,1],:]
-    #SW = points[1]
-    #NE = points[4]
-    xs = [pt[1] for pt in rectpoints]
-    ys = [pt[2] for pt in rectpoints]
+    rectpoints = points[[1,2,4,3],:]
+    xs = [pt[1] for pt in rectpoints];
+    ys = [pt[2] for pt in rectpoints];
     seriestype := :shape
-    xs, ys #this is what we are returning
+	s = Shape(xs[:], ys[:])
 end
-#rectangle(w, h, x, y) = Shape(x + [0,w,w,0], y + [0,0,h,h])
-
 
 
 sizePlot = (400,400)
-myrect = HyperRectangle(Vec(0.5, 0.5), Vec(1., 1))
-plot(myrect, legend=false, size=sizePlot, yaxis=((0,10), 0:1:10), xaxis=((0,10), 0:1:10))
+r = HyperRectangle(Vec(0.5, 0.5), Vec(1., 1))
+plot!(r, opacity=0.5, legend=false, size=sizePlot, yaxis=( (0,10), 0:1:10), xaxis=( (0,10), 0:1:10)) 
