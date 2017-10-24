d79ef32c98802cf84e7ff0f0f82a393e414a3474
Nancy Ouyang <nouyang@alum.mit.edu>
2017-10-24 17:30:54 -0400

    types

--- wk5/scratch.jl
+++ wk5/scratch.jl
@@ -1,13 +1,24 @@
+##  Conclusion: make my life easier and just define a GraphNode and a TreeNode
+
 print("Hi!")
 module tmp
     using GeometryTypes
     struct Node2
         id::Int
         iPrev::Int #parent
-        state::Point2f0
+        state::Nullable{Point2f0}
     end
 
-    Node2(id::Int, iPrev::Int) = Node2(id, iPrev, Point(2,2))
+    #Node2(id::Int, iPrev::Int) = Node2(id, iPrev, Point2f0())
+    Node2(id::Int, iPrev::Int) = Node2(id, iPrev, Point(999,999))
+    # Node2(id::Int, iPrev::Int) = Node2(id, iPrev, Point())	
+# 
+	# Fails with;
+	# WARNING: replacing module tmp
+	# ERROR: LoadError: MethodError: Cannot `convert` an object of type Tuple{} to an object of type Float32
+	# This may have arisen from a call to the constructor Float32(...),
+	# since type constructors fall back to convert methods.
+
 end
 
     x = tmp.Node2(1,2,Point(2,3))
