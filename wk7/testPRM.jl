699e689693896549f4482ef79cf15b84539ebf5a
Nancy Ouyang <nouyang@alum.mit.edu>
2017-11-07 03:57:22 -0500

    plotPRM does not return anything, removed erroneous assignment

--- wk7/testPRM.jl
+++ wk7/testPRM.jl
@@ -42,13 +42,17 @@ function main()
     startstate = Point(1.,1)
     goalstate = Point(18.,18)
 
+    #!
     pathcost, isPathFound, solPath = queryPRM(startstate, goalstate, nodeslist, edgeslist, obstacles)
 
     ## Plot path found
     title = "PRM with # samples =$numSamples, \nPathfound = $isPathFound, \npathcost = $pathcost"
+    #!
     roadmap = algT.roadmap(startstate, goalstate, nodeslist, edgeslist)
 
-    plot = plotfxn.plotPRM(roadmap, solPath, title::String)
+    # this is the gold
+    #!
+    plotfxn.plotPRM(roadmap, solPath, title::String)
 
 ####
 #startGoal = algT.GraphNode(0, Point(0,0))
